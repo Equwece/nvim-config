3703fc42ad8ce0227ac82ea10ea7ff7b98f8b176
Equwece <99554597+Equwece@users.noreply.github.com>
2023-08-22 12:00:40 +0400

    Add telescope.nvim

--- init.lua
+++ init.lua
@@ -337,9 +337,16 @@ require('packer').startup(function(use)
     branch = "master"
   }
 
+  -- Java LSP client plugin
   use {
     'https://github.com/mfussenegger/nvim-jdtls',
     branch = "master"
+  }
+
+  -- Find, Filter, Preview, Pick.
+  use {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.2',
   }
 end)
 
@@ -573,6 +580,7 @@ end
 
 NvimCmpSetup()
 
+-- JDTLS SETUP
 function NvimJdtlsSetup()
   -- See `:help vim.lsp.start_client` for an overview of the supported `config` options.
   local config = {
@@ -608,3 +616,60 @@ function NvimJdtlsSetup()
 end
 
 NvimJdtlsSetup()
+
+---- TELESCOPE SETUP
+
+function TelescopeSetup()
+  map('n', '<leader>ff', ':Telescope find_files hidden=true<CR>', default_opts)
+  map('n', '<leader>fg', ':Telescope live_grep<CR>', default_opts)
+  map('n', '<leader>fb', ':Telescope buffers<CR>', default_opts)
+  map('n', '<leader>fh', ':Telescope help_tags<CR>', default_opts)
+
+  local actions = require "telescope.actions"
+
+  require('telescope').setup {
+    defaults = {
+      -- Default configuration for telescope goes here:
+      -- config_key = value,
+      mappings = {
+        i = {
+          ["<esc>"] = require('telescope.actions').close,
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+        },
+        n = {
+          ["<C-j>"] = actions.move_selection_next,
+          ["<C-k>"] = actions.move_selection_previous,
+        }
+      },
+      vimgrep_arguments = {
+        'rg',
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--smart-case',
+        '--hidden'
+      },
+    },
+    pickers = {
+      -- Default configuration for builtin pickers goes here:
+      -- picker_name = {
+      --   picker_config_key = value,
+      --   ...
+      -- }
+      -- Now the picker_config_key will be applied every time you call this
+      -- builtin picker
+    },
+    extensions = {
+      -- Your extension configuration goes here:
+      -- extension_name = {
+      --   extension_config_key = value,
+      -- }
+      -- please take a look at the readme of the extension you want to configure
+    }
+  }
+end
+
+TelescopeSetup()
