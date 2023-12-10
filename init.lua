113a7c5d8925eda90b9874f74c5d659cacf6ef2b
Equwece <99554597+Equwece@users.noreply.github.com>
2023-12-11 00:02:33 +0400

    Fix dap ui layout

--- init.lua
+++ init.lua
@@ -739,21 +739,103 @@ TodoCommentsSetup()
 function DapUiSetup()
   local dapui = require('dapui')
   local dap = require('dap')
-  dapui.setup()
+  dapui.setup(
+    {
+      controls = {
+        element = "repl",
+        enabled = true,
+        icons = {
+          disconnect = "",
+          pause = "",
+          play = "",
+          run_last = "",
+          step_back = "",
+          step_into = "",
+          step_out = "",
+          step_over = "",
+          terminate = ""
+        }
+      },
+      element_mappings = {},
+      expand_lines = true,
+      floating = {
+        border = "single",
+        mappings = {
+          close = { "q", "<Esc>" }
+        }
+      },
+      force_buffers = true,
+      icons = {
+        collapsed = "",
+        current_frame = "",
+        expanded = ""
+      },
+      layouts = {
+        {
+          elements = {
+            {
+              id = "scopes",
+              size = 0.15
+            },
+            {
+              id = "breakpoints",
+              size = 0.15
+            },
+            {
+              id = "stacks",
+              size = 0.15
+            },
+            {
+              id = "watches",
+              size = 0.15
+            }
+          },
+          position = "left",
+          size = 30
+        },
+        {
+          elements = {
+            {
+              id = "repl",
+              size = 1
+            },
+            -- {
+            --   id = "console",
+            --   size = 0.5
+            -- }
+          },
+          position = "bottom",
+          size = 10
+        }
+      },
+      mappings = {
+        edit = "e",
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        repl = "r",
+        toggle = "t"
+      },
+      render = {
+        indent = 1,
+        max_value_lines = 100
+      }
+    }
+  )
 
   -- Add dap ui to dap
   dap.listeners.after.event_initialized["dapui_config"] = function()
     dapui.open()
-    dap.repl.close()
-    dap.repl.open()
   end
   dap.listeners.before.event_terminated["dapui_config"] = function()
-    dap.repl.close()
-    dapui.close()
+    -- Do not close ui and repl to examine errors and results
+
+    -- dapui.close()
   end
   dap.listeners.before.event_exited["dapui_config"] = function()
-    dap.repl.close()
-    dapui.close()
+    -- Do not close ui and repl to examine errors and results
+
+    -- dapui.close()
   end
 end
 
